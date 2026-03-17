6b2fb31f0d579e740229decfa87e50291e329d94
Muhammad Iqbal <muhammad.iqbal4115@gmail.com>
2026-03-17 22:48:16 +0500

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -46,7 +46,12 @@ NEON_CSS = """
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Rajdhani:wght@400;500;600;700&family=Share+Tech+Mono&family=Exo+2:wght@300;400;500;600&display=swap');
 
-#tabs-bui47-tabpanel-0 {
+# #tabs-bui47-tabpanel-0 {
+#     width: 50%;
+#     margin: 0 auto;
+# }
+
+div[data-testid="stVerticalBlock"] {
     width: 50%;
     margin: 0 auto;
 }
